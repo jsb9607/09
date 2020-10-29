c7e3a943d17672e9103f4641367e036ae95da4f2
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-10-29 15:57:10 +0900

    09_05

--- 01.c
+++ 01.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE	5
+#define ROWS	3
+#define COLS	3
 
+int addMarix(int x[][COLS], int y[][COLS], int z[][COLS])
+{	
+	for(int i=0; i<ROWS; i++)
+		for(int j=0; j<COLS; j++)
+			z[i][j] = x[i][j] + y[i][j];
+
+	return z[ROWS][COLS];
+}
+
+void printMatrix(int x[][COLS])
+{
+	
+	for(int i=0; i<ROWS; i++)
+	{
+		for(int j=0; j<COLS; j++)
+			printf("%d ", x[i][j]);
+		printf("\n");
+	}
+}
 
 int main(void) 
 {
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
-	
-	for(i=0; i<SIZE ; i++)
-	{
-		grade[i] = rand()%100;
-		score[i] = grade[i];
-	}
-	
-	for(i=0; i<SIZE ; i++)
-	{
-		printf("score[%d] = %d grade[%d] = %d\n", i, score[i], i, grade[i]);
-	}
+	int A[ROWS][COLS] = {{2,3,0},{8,9,1},{7,0,5}};	
+	int B[ROWS][COLS] = {{1,0,0},{0,1,0},{0,0,1}};
+	int C[ROWS][COLS];
+
+	addMarix(A, B, C);
+	printMatrix(C);
 	
 	return 0;
 }
+
 
 
