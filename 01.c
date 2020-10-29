65cd48c89e314703f0327459c054187bfc0537b4
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-10-29 15:45:52 +0900

    09_03

--- 01.c
+++ 01.c
@@ -6,26 +6,19 @@
 int main(void) 
 {
 	int i;
-	int grade[5];
-	int average;
-	int sum;
+	int grade[SIZE];
+	int score[SIZE];
 	
-	printf("5명의 점수를 입력하세요.\n");
-
 	for(i=0; i<SIZE ; i++)
 	{
-		scanf("%d", &grade[i]);
-		sum += grade[i];
+		grade[i] = rand()%100;
+		score[i] = grade[i];
 	}
 	
 	for(i=0; i<SIZE ; i++)
 	{
-		printf("grade[%d] = %d\n", i, grade[i]);
+		printf("score[%d] = %d grade[%d] = %d\n", i, score[i], i, grade[i]);
 	}
-
-	average = sum/SIZE;
-	printf("성적평균 : %d\n", average);
-
 	
 	return 0;
 }
